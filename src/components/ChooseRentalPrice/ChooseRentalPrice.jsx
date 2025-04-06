4b705660a8a4d01c7df332331bd5e42074b9ba6d
YD <yuriidiachuk@icloud.com>
2025-04-07 00:42:28 +0300

    add css to ChooseBrand and ChooseRentalPrice and make custom select

--- src/components/ChooseRentalPrice/ChooseRentalPrice.jsx
+++ src/components/ChooseRentalPrice/ChooseRentalPrice.jsx
@@ -1,35 +1,66 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import s from "./ChooseRentalPrice.module.css";
 import { useDispatch } from "react-redux";
 import { setRentalPrice } from "../../redux/filterSlice.js";
 
 const ChooseRentalPrice = () => {
   const prices = [30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
-
   const dispatch = useDispatch();
-  const handleChangePrice = (e) => {
-    dispatch(setRentalPrice(e.target.value));
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const dropdownRef = useRef(null);
+
+  const handleSelect = (price) => {
+    setSelected(price);
+    dispatch(setRentalPrice(price));
+    setIsOpen(false);
   };
+
+  const handleClickOutside = (e) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const formatSelectedPrice = (price) => {
+    if (price) {
+      return `To $${price}`;
+    }
+    return "Choose a price";
+  };
+
   return (
-    <div className={s.container}>
-      <select
-        onChange={handleChangePrice}
-        className={s.selectRentalPrice}
-        name="rentalPrice"
-        id="rentalPrice"
-      >
-        <option value="">Choose a price</option>
-        {prices.map((price) => {
-          return (
-            <option key={price} value={price}>
+    <div className={s.container} ref={dropdownRef}>
+      <div className={s.selectBox} onClick={() => setIsOpen(!isOpen)}>
+        <span className={s.selectedText}>{formatSelectedPrice(selected)}</span>
+        <svg
+          className={`${s.svg} ${isOpen ? s.rotate : ""}`}
+          width="16"
+          height="16"
+        >
+          <use href="/assets/sprite.svg#icon-vector-upcopy"></use>
+        </svg>
+      </div>
+
+      {isOpen && (
+        <ul className={`${s.dropdown} ${isOpen ? s.dropdownVisible : ""}`}>
+          {prices.map((price) => (
+            <li
+              key={price}
+              className={s.option}
+              onClick={() => handleSelect(price)}
+            >
               {price}
-            </option>
-          );
-        })}
-      </select>
-      <svg className={s.svg} width="16" height="16">
-        <use href="/assets/sprite.svg#icon-vector-upcopy"></use>
-      </svg>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
