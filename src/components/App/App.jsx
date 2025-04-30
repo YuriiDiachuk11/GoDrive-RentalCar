811cb73707eee54a9ee744efdec0a81e9ae8034a
YD <yuriidiachuk@icloud.com>
2025-04-30 16:48:50 +0300

    update App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,8 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "../Layout/Layout.jsx";
 import Loader from "../Loader/Loader.jsx";
 import { ToastContainer } from "react-toastify";
-import CarFavoriteListPage from "../../pages/CarFavoriteListPage/CarFavoriteListPage.jsx";
-import PageNotFound from "../../pages/NotFoundPage/NotFoundPage.jsx";
 
 const HomePage = React.lazy(() => import("../../pages/HomePage/HomePage.jsx"));
 const CarCatalogPage = React.lazy(() =>
@@ -12,6 +10,12 @@ const CarCatalogPage = React.lazy(() =>
 );
 const CarFavouritePage = React.lazy(() =>
   import("../../pages/CarFavouritePage/CarFavouritePage.jsx")
+);
+const CarFavoriteListPage = React.lazy(() =>
+  import("../../pages/CarFavoriteListPage/CarFavoriteListPage.jsx")
+);
+const PageNotFound = React.lazy(() =>
+  import("../../pages/NotFoundPage/NotFoundPage.jsx")
 );
 
 const App = () => {
@@ -22,43 +26,17 @@ const App = () => {
         autoClose={5000}
         hideProgressBar={false}
       />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={
-              <Suspense fallback={<Loader />}>
-                <HomePage />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/catalog"
-            element={
-              <Suspense fallback={<Loader />}>
-                <CarCatalogPage />
-              </Suspense>
-            }
-          />
-          <Route
-            path="/catalog/:id"
-            element={
-              <Suspense fallback={<Loader />}>
-                <CarFavouritePage />
-              </Suspense>
-            }
-          />
-        </Route>
-        <Route
-          path="/favorites"
-          element={
-            <Suspense fallback={<Loader />}>
-              <CarFavoriteListPage />
-            </Suspense>
-          }
-        />
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+            <Route path="/catalog" element={<CarCatalogPage />} />
+            <Route path="/catalog/:id" element={<CarFavouritePage />} />
+          </Route>
+          <Route path="/favorites" element={<CarFavoriteListPage />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
